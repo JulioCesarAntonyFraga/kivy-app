caf343b4b94424b2494c96de105d57f975e82fc7
GabrielFragaM <gabriellff130@gmail.com>
2020-12-08 19:59:03 -0300

    update20

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sqlite3 as sql
+import sqlite3
+import random
+from sqlite3 import Error
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen,ScreenManager
@@ -5,7 +9,8 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton, MDRaisedButton
 from kivy.storage.jsonstore import JsonStore
 from kivymd.uix.picker import MDDatePicker
-from kivy.core.window import Window
+from datetime import date
+from kivymd.utils import asynckivy
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.card import MDCardSwipe
@@ -93,11 +98,10 @@ class PawareApp(MDApp):
         self.strng = Builder.load_file('conteudos.kv')
         return self.strng
     #############FUNCAO AO INICIAR O APP ELE VAI CARREGAR ISSO ANTES DE MOSTRAR TELA#################
-    def on_start(self):             
+    def on_start(self):         
         self.store = JsonStore("userProfile.json")
         try:
             if self.store.get('UserInfo')['name'] != "":
-                self.load_checklists_screen_main()
                 self.strng.get_screen('screen1').manager.current = 'screen1'
         except KeyError:
             self.strng.get_screen('welcomescreen').manager.current = 'welcomescreen' 
@@ -185,8 +189,9 @@ class PawareApp(MDApp):
         self.change_screen_to_checklists()
     #################REMOVE WIDGET CHECKLIST##################
     def remove_checklist(self):
-        self.strng.get_screen('screen2').ids.my_checklists.remove_widget(self.list_name)
-        print(self.list_name)
+        self.strng.get_screen('screen2').ids.my_checklists.remove_widget(self.id_nome)
+        print(self.id_nome)
+
     
     ############MUDANDO A TELA PARA CHECKLIST INFORMAÇOES##########
     def change_screen(self, ThreeLineIconListItem):
@@ -211,14 +216,59 @@ class PawareApp(MDApp):
         date_dialog.open()
 
     ###############CARREGANDO OS VALORES DO PERFIL##############
-    try:
-        store = JsonStore("userProfile.json")
-        nome_perfil = store.get('UserInfo')['name']
-        email_perfil = store.get('UserInfo')['email']
-    except:
-        nome_perfil = "Usuário desconhecido"
-        email_perfil = "Email não cadastrado"
+    def save_edits_profile(self,):
+        nome = self.strng.get_screen('profile').ids.profile_name_input.text
+        email = self.strng.get_screen('profile').ids.profile_email_input.text
+        id_name = self.strng.get_screen('profile').ids.profile_id_input.text
+        try:
+            con = sql.connect('database.db')
+            cur = con.cursor()
+            cur.execute("UPDATE profile SET Name=?, Email=?  WHERE Name_id =?", (nome, email,id_name))
+            con.commit()
+            con.close()
+        except Error as erro:
+            print(erro)
 
+
+    try:
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("SELECT Name_id, Name, Email from profile")
+        for row in cur:
+            id_nome = str(row[0])
+            name_perfil_toolbar = row[1]
+            email_perfil_toolbar = row[2]
+        con.commit()
+        con.close()
+    except:
+        pass
+    
+    id_nome = 'str(row[0])'
+    name_perfil_toolbar = 'row[1]'
+    email_perfil_toolbar = 'row[2]'
+
+    
+
+    #########FUNCAO RECARREGAR OS DELETALHES DO PEFIL APOS MUDANÇA##############
+    def set_refresh_screen1(self):
+        async def set_refresh_screen1():
+            for i in range(1):
+                await asynckivy.sleep(1)
+                try:
+                    con = sql.connect('database.db')
+                    cur = con.cursor()
+                    cur.execute("SELECT Name_id, Name, Email from profile")
+                    for row in cur:
+                        self.strng.get_screen('screen1').ids.name_perfil_toolbar.text = row[1]
+                        self.strng.get_screen('screen1').ids.email_perfil_toolbar.text = row[2]
+                        print('teste')
+                    con.commit()
+                    con.close()
+                except:
+                    pass
+        asynckivy.start(set_refresh_screen1())
+
+        
     ###############CARREGANDO OS VALORES DA CHECKLIST##############
     try:
         store = JsonStore("dataChecklist.json")
@@ -260,21 +310,48 @@ class PawareApp(MDApp):
                 cancel_btn_username_dialogue = MDFlatButton(text='OK',on_release = self.close_username_dialogue)
                 self.dialog = MDDialog(title = 'Email inválido',text = "Por favor preencha um email válido",size_hint = (0.7,0.2),buttons = [cancel_btn_username_dialogue])
                 self.dialog.open()
+
+    ######BUSCANDO VALORES DEPOIS DE PREENCHER A LV############
+    def get_details_lv(self):
+        Id_checklist = 1
+        Name_checklist = self.strng.get_screen('checklistName').ids.name_text_fied_lv.text
+        Descricao = self.strng.get_screen('checklistName').ids.descricao_text_fied.text
+
+        Acao = self.strng.get_screen('checklistItem1').ids.acao_item1.text
+        Responsavel_realizar = self.strng.get_screen('checklistItem1').ids.responsavel_item1.text
+        Prazo = self.strng.get_screen('checklistItem1').ids.prazo_item1.text
+
+        Conformes = 8
+        Nao_conformes = 1
+        Nao_aplicaveis = 0
+        Total_resultado = '99 porcento'
+        Status = 'PENDENTE'
+        Criado_por = self.name_perfil_toolbar
+        data_atual = date.today()
+        Data_criada = str('{}/{}/{}'.format(data_atual.day, data_atual.month,data_atual.year))
+
+        self.save_new_checklist_screen(Id_checklist, Name_checklist, Criado_por, Data_criada,Acao,Responsavel_realizar,Prazo,Conformes,Nao_conformes, Nao_aplicaveis, Total_resultado,Descricao, Status)
+
+
+    def save_new_checklist_screen(self,Id_checklist,Name_checklist,Criado_por,Data_criada,Acao,Responsavel_realizar,Prazo,Conformes, Nao_conformes, Nao_aplicaveis, Total_resultado,Descricao, Status):
+        try:
+            self.id_nome = str(Id_checklist)
+            self.id_nome = ThreeLineIconListItem(
+            text=Name_checklist,
+            secondary_text='Responsável: ' + Criado_por,
+            tertiary_text='Data de emissão: ' + Data_criada, on_release=self.change_screen)
+            self.id_nome.add_widget(IconLeftWidget(icon='check-box-outline'))
+            self.strng.get_screen('screen2').ids.my_checklists.add_widget(self.id_nome)
+            self.strng.get_screen('screen1').manager.current = 'screen1'
+            con = sql.connect('database.db')
+            cur = con.cursor() 
+            cur.execute("INSERT INTO checklist VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",(Id_checklist,Name_checklist,Criado_por,Data_criada,Acao,Responsavel_realizar,Prazo,Conformes, Nao_conformes, Nao_aplicaveis, Total_resultado,Descricao, Status))
+        except Exception as erro:
+            print(erro)
+
         
-        ######BUSCANDO NO BANCO AS CHECKLIST############
-    def load_checklists_screen_main(self):
-        store = JsonStore("dataChecklist.json")
-        self.list_name = store.get('UserInfo')['name']
-        criado_por = store.get('UserInfo')['data']
-        criado_em = store.get('UserInfo')['responsavel']
-        
-        self.list_name = ThreeLineIconListItem(
-        text=self.list_name,
-        secondary_text='Responsável: ' + criado_por,
-        tertiary_text='Data de emissão: ' + criado_em, on_release=self.change_screen)
-        self.list_name.add_widget(IconLeftWidget(icon='check-box-outline'))
-        self.strng.get_screen('screen2').ids.my_checklists.add_widget(self.list_name)
-        self.strng.get_screen('screen1').manager.current = 'screen1'
+           
+    
 
     
     ####################FUNCAO DE BLOQUEI DOS BOTAO CASO NAO SEJA SELECIONADO AS OPCOES DA VERIFICAÇAO############
